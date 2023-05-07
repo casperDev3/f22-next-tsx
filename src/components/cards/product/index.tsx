7a45a0e58b31cda16a625eb937addc9af99420a3
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-05-07 09:51:15 +0300

    add summury

--- src/components/cards/product/index.tsx
+++ src/components/cards/product/index.tsx
@@ -6,17 +6,18 @@ import s from "./index.module.scss";
 const Product = (props: any) => {
   const { product } = props;
   const { title, price, image } = product;
+  console.log(product);
   return (
     <>
       <div className={s.products__card}>
         <div className={s.products__card_img}>
-          {/* <Image src={image} alt="product" width={200} height={200} /> */}
+          <Image src={image} alt="product" width={200} height={200} />
         </div>
         <div className={s.products__card_title}>
           {title.length > 20 ? title.slice(0, 20) + "..." : title}
         </div>
         <div className={s.products__card_price}>{price}</div>
-        <div className={s.products__card_button}>Add to cart</div>
+        <div className="btn btn-primary">Add to cart</div>
       </div>
     </>
   );
